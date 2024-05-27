9c54ecd7b07daabf480dbf61038309f617a7e595
Neo Aylor <neoyukiaylor@gmail.com>
2024-05-27 14:35:47 +0100

    Fixed wrong link

--- Windows/PowerShell/Spicetify-Installer/bin/Spicetify-Installer.bat
+++ Windows/PowerShell/Spicetify-Installer/bin/Spicetify-Installer.bat
@@ -1,1 +1,1 @@
-PowerShell.exe -Command "Invoke-Expression (Invoke-RestMethod -UseBasicParsing  -URI 'https://gitlab.com/___Neopolitan/Scripts/-/raw/main/Windows/PowerShell/Shutdown-Scheduler/bin/Shutdown-Scheduler.ps1')"
+PowerShell.exe -Command "Invoke-Expression (Invoke-RestMethod -UseBasicParsing  -URI 'https://gitlab.com/___Neopolitan/Scripts/-/raw/main/Windows/PowerShell/Spicetify-Installer/bin/Spicetify-Installer.ps1')"
