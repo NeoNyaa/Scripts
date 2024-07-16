1795345346c949d73df3bf2e93c8e07624e6e0e0
Neo Aylor <neoyukiaylor@gmail.com>
2024-07-16 07:09:27 +0100

    prep

---
+++ Windows/PowerShell/Impersonate-TrustedInstaller/bin/Impersonate-TrustedInstaller.ps1
@@ -0,0 +1,23 @@
+# Clean up the terminal and check if PowerShell was ran with administrator permissions.
+Clear-Host
+
+if ([bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544") -eq $False) {
+    Read-Host -Prompt "`nERROR! PowerShell was not ran with administrator Permissions"
+    exit 1
+}
+
+# Install and Import a PowerShell module which makes this possible.
+Write-Host "`nInstalling the NtObjectManager PowerShell module."
+Install-Module -Name NtObjectManager -Force -Scope CurrentUser -WarningAction 'SilentlyContinue'
+Import-Module NtObjectManager
+
+# Impersonate TrustedInstaller
+Write-Host "Attempting to impersonate TrustedInstaller"
+Start-Service TrustedInstaller
+try {
+    $null = (Get-NtThread -Current -PseudoHandle).ImpersonateThread((Get-NtProcess -Name TrustedInstaller.exe).GetFirstThread())
+    Write-Host "Success!"
+}
+catch {
+    Write-Host "Failed! Make sure that only one instance of an impersonated terminal session is active at any one time."
+}
--- Windows/PowerShell/Spicetify-Installer/README.md
+++ Windows/PowerShell/Spicetify-Installer/README.md
@@ -1,5 +1,8 @@
 # [Download Spicetify Installer](https://gitlab.com/___Neopolitan/Scripts/-/raw/main/Windows/PowerShell/Spicetify-Installer/bin/Spicetify-Installer.bat?inline=false)
 
+# <a href="https://raw.githubusercontent.com/NeoNyaa/Scripts/main/Windows/PowerShell/Neo-ToolKit/bin/Neo-ToolKit.bat">Download Spicetify Installer</a>
+
 ### Changelog
-- v1.0.0
-    - Initial Release
+
+-   v1.0.0
+    -   Initial Release
