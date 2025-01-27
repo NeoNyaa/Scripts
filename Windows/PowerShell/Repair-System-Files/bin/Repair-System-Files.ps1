e2ffa1f52fd26853884e54d50cf86a834504236d
Neo Yuki Aylor <neoyukiaylor@gmail.com>
2025-01-27 07:23:42 +0000

    Fixed error

--- Windows/PowerShell/Repair-System-Files/bin/Repair-System-Files.ps1
+++ Windows/PowerShell/Repair-System-Files/bin/Repair-System-Files.ps1
@@ -1,4 +1,4 @@
 SFC.exe /ScanNow
-DISM.exe /Online /Cleanup-Wim
-DISM.exe /Online /Cleanup-MountPoints
+DISM.exe /Cleanup-Wim
+DISM.exe /Cleanup-MountPoints
 DISM.exe /Online /Cleanup-Image /RestoreHealth
